fb100af3bc0901dfe803755c9a3726a5266b6df7
Darius K <darius.kaya@gmail.com>
2024-04-06 21:33:13 +0200

    hamburger menu done

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -5,23 +5,44 @@ import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
 import styles from './Layout.module.css'
 import Footer from '../../components/Footer';
+import hamburgerMenu from '../../assets/images/hamburger-menu.png'
 
 const Layout = ({ updateSelectedGroup, pageTitle, showAnimal, resetShowAnimal }) => {
+
+    const  [showHamburger, setShowHamburger] = useState(false)
  
     const handlePageChoice = (group) => {
         updateSelectedGroup(group)
         resetShowAnimal(null)
+        setShowHamburger(false)
     }
 
+    const toggleHamburger = () => {
+        setShowHamburger(!showHamburger)
+    }
+
+    const hideHamburger = () => {
+        setShowHamburger(false)
+    }
+ 
     return (
         <>
             <nav className={styles.nav}>
                 <img className={styles.logo} src={zooLogo} alt="logo" width="60px" />
+                <img onClick={toggleHamburger} className={`${styles.hamburgerImg} ${styles[showHamburger ? 'spinBurger' : '']}`} src={hamburgerMenu} alt="hamburger" width="30px" />
+
                 <NavLink onClick={() => handlePageChoice(null)} className={styles.navItem} to="/">Home</NavLink>
                 <NavLink onClick={() => handlePageChoice('mammals')} className={styles.navItem} to="/mammals?group=mammals">Mammals</NavLink>
                 <NavLink onClick={() => handlePageChoice('birds')} className={styles.navItem} to="/birds?group=birds">Birds</NavLink>
                 <NavLink onClick={() => handlePageChoice('reptiles')} className={styles.navItem} to="/reptiles?group=reptiles">Reptiles</NavLink>
             </nav>
+            <nav className={`${styles.hamburger} ${styles[showHamburger ? 'visible' : '']}`}>
+                <NavLink onClick={() => handlePageChoice(null)} className={styles.hamburgerItem} to="/">Home</NavLink>
+                <NavLink onClick={() => handlePageChoice('mammals')} className={styles.hamburgerItem} to="/mammals?group=mammals">Mammals</NavLink>
+                <NavLink onClick={() => handlePageChoice('birds')} className={styles.hamburgerItem} to="/birds?group=birds">Birds</NavLink>
+                <NavLink onClick={() => handlePageChoice('reptiles')} className={styles.hamburgerItem} to="/reptiles?group=reptiles">Reptiles</NavLink>
+            </nav>
+            <div onClick={hideHamburger} className={`${styles.overlay} ${styles[showHamburger ? 'showOverlay' : '']}`}></div>
             <Header pageTitle={pageTitle} />
             <div className={styles.mainContent}>
                 <Sidebar showAnimal={showAnimal}/>
